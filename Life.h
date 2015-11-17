354f73d2a0fbf79294981eefdff1b4ca9963dafd
HowardDennis <h2dennis@gmail.com>
2015-11-16 20:36:41 -0600

    Create Life.h

---
+++ Life.h
@@ -0,0 +1,20 @@
+#include <cassert>	// assert
+#include <string>	// string
+#include <vector>	// vector
+#include <iostream> // endl, istream, ostream
+#include <sstream>  // istringstream
+#include <cstdlib>	// rand
+
+using namespace std;
+
+class AbstractCell {
+protected:
+  // -----
+  // alive
+  // -----
+	/**
+	 * Stores cell's lif
+	 */
+	 bool alive;
+	 
+	 }
